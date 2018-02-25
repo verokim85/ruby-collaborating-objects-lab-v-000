da6214923eb053a1981509a28115fc173c51b085
Veronica Kim <vero.a.kim@gmail.com>
2018-02-25 01:21:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ def self.new_by_filename(filename)
   artist = filename.split(" - ")[0]
   name = filename.split(" - ")[1]
   song = Song.new(name)
+  song.artist_name = artist
   song.artist = Artist.new(artist)
   song
 
