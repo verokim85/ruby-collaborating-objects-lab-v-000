86e6f58b0f95a1092a1098b2d6b9a10b830f9b29
Veronica Kim <vero.a.kim@gmail.com>
2018-02-25 01:18:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ end
 
 def artist_name=(name)
   self.artist = Artist.find_or_create_by_name(name)
-  self.artist = add_song(name)
+  self.artist = add_song(self)
 end
 
 def self.new_by_filename(filename)
